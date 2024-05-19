44428733655d824de9bab5b193920582fc3066fe
TPMPabasara9 <140165387+TPMPabasara9@users.noreply.github.com>
2024-05-19 17:21:55 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,23 @@
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Projects from './pages/Projects';
+import About from './pages/About';
+
 export default function App() {
   return (
-    <h1 className='text-3xl text-green text-center text-pink-600'  >Welcome to web development</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} /> {/* Corrected path for About page */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,7 @@
+
+
+export default function Dashbooard() {
+  return (
+    <div>Dashbooard</div>
+  );
+}
