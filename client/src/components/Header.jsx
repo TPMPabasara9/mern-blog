0f187e741187ad1b9182cb97a40970443a1c0288
TPMPabasara9 <140165387+TPMPabasara9@users.noreply.github.com>
2024-05-26 19:16:44 +0530

    create signup page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -14,10 +14,11 @@ function Header() {
         <div className="container mx-auto flex justify-between items-center px-4">
           <Link to="/" className="flex items-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-              Madusha'
+              Madusha's
             </span>
-            <span className="ml-2">Blog</span>
+            Blog
           </Link>
+          <div className='flex-grow mx-4'>
           <form className="hidden lg:flex items-center ml-4">
             <TextInput
               type="text"
@@ -26,6 +27,7 @@ function Header() {
               className='lg:inline'
             />
           </form>
+          </div>
           </div>
           <div className="container mx-auto px-4 flex justify-end">  {/* Added justify-end for right alignment */}
           <div className="flex items-center gap-4">  {/* Increased gap between menu items */}
@@ -47,7 +49,7 @@ function Header() {
               <FaMoon />
             </Button>
             <Link to="/signin">
-              <Button className="bg-purple-950" outline>
+              <Button gradientDuoTone="purpleToBlue" outline>
                 Sign In
               </Button>
             </Link>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,21 @@
+const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports={
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
-  plugins: [require('flowbite/plugin'),
-            require('tailwind-scrollbar'),
+  plugins: [
+    flowbite.plugin(),
+    require('flowbite/plugin'),
+
+
+            
   ],
   
 };
