d5c987b05fe9168defcf4ba790f2e0ed33c74207
TPMPabasara9 <140165387+TPMPabasara9@users.noreply.github.com>
2024-05-27 15:37:19 +0530

    add functionality to signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,49 @@
-import { TextInput ,Label, Button} from "flowbite-react";
-import { Link } from "react-router-dom";
+import { TextInput ,Label, Button, Alert, Spinner} from "flowbite-react";
+import { useState } from "react";
+import { Link,useNavigate } from "react-router-dom";
+
 
 export default function Signup() {
-  return (
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage]= useState(null);
+  const [loading,setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) =>{
+    setFormData({...formData,[e.target.id]:e.target.value.trim() });
+  };
+
+  const handleSubmit= async (e) =>{
+    e.preventDefault();
+    if(!formData.username || !formData.email || !formData.password){
+      return setErrorMessage('Please fill out all fields.');
+    }
+    try{
+      setLoading(true);
+      setErrorMessage(null);
+      const res= await fetch('/api/auth/signup',{
+        method:'POST',
+        headers: {'Content-Type' :'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data =await res.json();
+      setLoading(false);
+      if(res.ok){
+        navigate('/signin');
+      }
+      if(data.success===false) {
+        return setErrorMessage(data.message);
+      }
+         
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+   
+    }
+  };
+
+  
+ return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row 
       md:items-center gap-5">
@@ -18,7 +59,7 @@ export default function Signup() {
             </span>
             Blog
           </Link>
-          <p className='text-sm mt-5' >
+          <p className='text-sm mt-5 font-semibold' >
             This is a demo project. you can sign up with your email and
             password
             or with Google.
@@ -27,31 +68,45 @@ export default function Signup() {
         <div className=""></div>
         {/*right */}
         <div className='flex -1'>
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit} >
             <div >
               <Label value='Your username' />
-              <TextInput type="text" placeholder="Username" id='username' />
+              <TextInput type="text" placeholder="Username" id='username' onChange={handleChange}  />
             </div>
             <div>
               <Label value="Your email " />
-              <TextInput type='text' placeholder="example@gmail.com" id='email' />
-            </div>
+              <TextInput type='email' placeholder="example@gmail.com" id='email' onChange={handleChange} />              
+              </div>
             <div>
               <Label value='Your password '/>
-              <TextInput type='text' placeholder="Password" id='password' />
+              <TextInput type='password' placeholder="Password" id='password' onChange={handleChange}  />
             </div>
-            <Button gradientDuoTone="purpleToPink" type='submit'>
-              Sign Up
+            <Button gradientDuoTone="purpleToPink" type='submit' disabled={loading}>
+              {
+                loading ? (
+                  <>      <Spinner size='sm'/>
+                  <span className="pl-3">Loading...</span></>
+            
+                ): 'Sign Up'
+              }
+              
               </Button>
           <div className="flex gap-2 text-sm mt-5">
             <span>Have an account?</span>
             <Link to ='/sign-in' className="text-blue-500 hover:underline">
               Sign In
             </Link>
+
           </div>
+          {
+            errorMessage &&(
+              <Alert className="mt-5" color='failure'>
+                {errorMessage}
+              </Alert>
+            )
+          }
           </form>
         </div>
-        <div className=""></div>
       </div>
     </div>
   );
